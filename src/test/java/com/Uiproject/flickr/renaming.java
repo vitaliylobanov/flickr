09b5499fefdde08e556c2625cddb49ab18b1a066
vitaliylobanov <vitaliy.lobanov@yahoo.com>
2015-01-18 22:01:04 -0800

    add some crap

--- src/test/java/com/Uiproject/flickr/renaming.java
+++ src/test/java/com/Uiproject/flickr/renaming.java
@@ -1,7 +1,6 @@
 package com.Uiproject.flickr;
 
-
-
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import javax.ws.rs.client.Client;
@@ -21,52 +20,61 @@ import org.openqa.selenium.support.ui.Select;
 import com.Uiproject.flickr.APIprop;
 
 
-
 public class renaming {
 	public static void main(String[] args) {
-		  WebDriver driver;
-          String baseUrl;
-          WebElement shit;
-			 Client client;
-					client = ClientBuilder.newClient();
-					String FlickrUrl = APIprop.flickrurl;
-						Invocation.Builder invocationBuilder = client
-								.target(FlickrUrl).request(MediaType.APPLICATION_JSON);
-						Response response = invocationBuilder.get();
-						String content = response.readEntity(String.class);
-						System.out.println(content);	
-						JSONObject jsonresponse = XML.toJSONObject(content).getJSONObject("rsp").getJSONObject("photos").getJSONObject("photo");
-						
-		           String title = jsonresponse.getString("title");
-		        
-		            
-		              driver = new FirefoxDriver();
-		              baseUrl = "http://www.airliners.net";
-		              driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		         
-		              driver.get(baseUrl + "/");
-		              driver.findElement(By.id("q")).click();
-		              driver.findElement(By.id("q")).clear();
-		              driver.findElement(By.id("q")).sendKeys(title);
-		              driver.findElement(By.name("submit")).click();
-		           
-		              new Select(driver.findElement(By.name("sort_order"))).selectByVisibleText("Latest Additions First");
-		              driver.findElement(By.cssSelector("option[value=\"photo_id desc\"]")).click();
-		             
-//		              driver.findElement(By.cssSelector("img[alt=\"Click here for full size photo!\"]")).click();
-//		              driver.findElement(By.xpath("//html/body/div[1]/div[3]/div/table[1]/tbody/tr[2]/td[2]/div[3]/table[2]/tbody/tr/td/table/tbody/tr[2]/td[1]/table/tbody/tr/td/font/a[1]"));
-//		              driver.findElement(By.xpath("(//html/body/div[1]/div[3]/div/table[1]/tbody/tr[2]/td[2]/div[3]/table[2]/tbody/tr/td/table/tbody/tr[2]/td[1]/table/tbody/tr/td/   
-		
-		             // /html/body/div[1]/div[3]/div/table[2]/tbody/tr[2]/td[1]/div[1]/table/tbody/tr[1]/td/table/tbody/tr[2]/td[2]/table/tbody/tr/td/font/a[1]
+		WebDriver driver;
+		String baseUrl;
+		Client client;
+		client = ClientBuilder.newClient();
+		String FlickrUrl = APIprop.flickrurl;
+		Invocation.Builder invocationBuilder = client.target(FlickrUrl)
+				.request(MediaType.APPLICATION_JSON);
+		Response response = invocationBuilder.get();
+		String content = response.readEntity(String.class);
+		System.out.println(content);
+		JSONObject jsonresponse = XML.toJSONObject(content)
+				.getJSONObject("rsp").getJSONObject("photos")
+				.getJSONObject("photo");
+
+		String title = jsonresponse.getString("title");
+
+		driver = new FirefoxDriver();
+		baseUrl = "http://www.airliners.net";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		driver.get(baseUrl + "/");
+		driver.findElement(By.id("q")).click();
+		driver.findElement(By.id("q")).clear();
+		driver.findElement(By.id("q")).sendKeys(title);
+		driver.findElement(By.name("submit")).click();
+
+		new Select(driver.findElement(By.name("sort_order")))
+				.selectByVisibleText("Latest Additions First");
+		driver.findElement(By.cssSelector("option[value=\"photo_id desc\"]"))
+				.click();
+		List<WebElement> resultList = driver
+				.findElements(By
+						.xpath(".//html/body/div/div[3]/div/table[2]/tbody/tr[2]/td[1]/div[1]/table/tbody/tr[1]/td/table/tbody/tr[2]/td[2]/table/tbody/tr/td/font/a[1]"));
+		for (WebElement resultItem : resultList) {
+			String tabname = resultItem.getText();
+			System.out.println(tabname);
+		}
+
+		driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+		driver.quit();
+
 	}
 }
 
+// driver.findElement(By.cssSelector("img[alt=\"Click here for full size photo!\"]")).click();
+// driver.findElement(By.xpath("//html/body/div[1]/div[3]/div/table[1]/tbody/tr[2]/td[2]/div[3]/table[2]/tbody/tr/td/table/tbody/tr[2]/td[1]/table/tbody/tr/td/font/a[1]"));
+// driver.findElement(By.xpath("(//html/body/div[1]/div[3]/div/table[1]/tbody/tr[2]/td[2]/div[3]/table[2]/tbody/tr/td/table/tbody/tr[2]/td[1]/table/tbody/tr/td/
 
-//Done! Here's the API key and secret for your new app:
-//	my app
+// Done! Here's the API key and secret for your new app:
+// my app
 //
-//Key:
-//86d9143b3942339fba26947499608870
+// Key:
+// 86d9143b3942339fba26947499608870
 //
-//Secret:
-//75ed9c6d7095f621 
+// Secret:
+// 75ed9c6d7095f621 
